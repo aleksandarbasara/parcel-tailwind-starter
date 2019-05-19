2b6753ab68e3c73511fab7b286877ddd00c4d2ec
Aleksandar Basara <aleksandar.basara@gmail.com>
2019-05-19 17:55:12 +0200

    PurgeCSS update

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,24 @@
-var tailwindcss = require('tailwindcss');
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
 
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'),
+        require('tailwindcss'),
         require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ]
 }
